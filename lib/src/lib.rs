7b834b01cd4f9faab5b952b86101bad808c87b30
Jean Casademont <jean.casademont@gmail.com>
2018-02-09 23:05:35 +0100

    Set 1.1: Hex to Base64

---
+++ lib/src/lib.rs
@@ -0,0 +1,25 @@
+extern crate base64;
+extern crate hex;
+
+pub fn hex2base64(input: &str) -> Result<String, hex::FromHexError> {
+    let b = hex::decode(input);
+    match b {
+        Ok(r) => return Ok(base64::encode(&r)),
+        Err(err) => return Err(err),
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_hex2base64() {
+        assert_eq!(
+            hex2base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d").unwrap(),
+            "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+        );
+    }
+}
