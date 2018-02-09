4aff6cda4ccc4b89b5d412a63e92de4a8c17f1ab
Jean Casademont <jean.casademont@gmail.com>
2018-02-09 23:39:28 +0100

    Set 1.2: XOR hexa strings

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -6,6 +6,12 @@ pub fn hex2base64(input: &str) -> Result<String, hex::FromHexError> {
     b.map(|r| base64::encode(&r))
 }
 
+pub fn xor(a: &str, b: &str) -> Result<Vec<u8>, hex::FromHexError> {
+    let l = try!(hex::decode(a));
+    let r = try!(hex::decode(b));
+    Ok(l.iter().zip(r.iter()).map(|(x, y)| x ^ y).collect())
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -17,5 +23,13 @@ mod tests {
             hex2base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d").unwrap(),
             "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
         );
+    }
+
+    #[test]
+    fn test_xor() {
+        assert_eq!(
+            hex::encode(xor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965").unwrap()),
+            "746865206b696420646f6e277420706c6179"
+        )
     }
 }
