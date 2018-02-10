89f0222ec7abbf6ea61f8d0928687dbc49a24d3f
Jean Casademont <jean.casademont@gmail.com>
2018-02-10 19:13:31 +0100

    Set 1.5: Repeating-key XOR cipher

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -74,6 +74,12 @@ pub fn detect_single_xor(input: &str) -> Result<String, hex::FromHexError> {
     Ok(candidate_string)
 }
 
+pub fn repeating_xor_cipher(input: &str, key: &str) -> Vec<u8> {
+   let input = input.as_bytes();
+   let key = key.bytes().cycle().take(input.len()).collect::<Vec<u8>>();
+   xor_bytes(input, &key)
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -111,5 +117,13 @@ mod tests {
         f.read_to_string(&mut contents).expect("something went wrong reading the file");
 
         println!("{}", detect_single_xor(&contents).unwrap());
+    }
+
+    #[test]
+    fn test_repeating_xor_cipher() {
+        assert_eq!(
+            hex::encode(repeating_xor_cipher("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal", "ICE")),
+            "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+        )
     }
 }
